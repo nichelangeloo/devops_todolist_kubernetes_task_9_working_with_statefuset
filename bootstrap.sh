4c61dff45e8aba30b37a9e78355829226d6da997
Nikita Ivanichenko <importantthings1482@gmail.com>
2026-06-08 00:20:16 +0300

    another fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,32 +1,23 @@
 #!/bin/bash
 
-echo "Creating namespaces"
-kubectl apply -f ".infrastructure/namespace.yml"
-kubectl create namespace mysql
- 
-echo "Deploying MySQL secrets"
-kubectl apply -f ".infrastructure/st-secret.yml"
- 
-echo "Deploying MySQL ConfigMap (init.sql)"
-kubectl apply -f ".infrastructure/st-configMap.yml"
- 
-echo "Deploying MySQL headless Service"
-kubectl apply -f ".infrastructure/st-service.yml"
- 
-echo "Deploying MySQL StatefulSet"
-kubectl apply -f ".infrastructure/statefulSet.yml"
- 
-echo "Deploying todoapp Secret"
-kubectl apply -f ".infrastructure/secret.yml"
- 
-echo "Deploying todoapp ConfigMap"
-kubectl apply -f ".infrastructure/configMap.yml"
+kind create cluster --config .infrastructure/cluster.yml
 
-echo "Deploying todoapp PV" 
-kubectl apply -f ".infrastructure/pv.yml"
-
-echo "Deploying todoapp PVC"
-kubectl apply -f ".infrastructure/pvc.yml"
+kubectl apply -f .infrastructure/namespace.yml
  
-echo "Deploying todoapp Deployment"
-kubectl apply -f ".infrastructure/deployment.yml"
+kubectl apply -f .infrastructure/st-secret.yml
+ 
+kubectl apply -f .infrastructure/st-configMap.yml
+ 
+kubectl apply -f .infrastructure/st-service.yml
+ 
+kubectl apply -f .infrastructure/statefulSet.yml
+ 
+kubectl apply -f .infrastructure/secret.yml
+ 
+kubectl apply -f .infrastructure/configMap.yml
+
+kubectl apply -f .infrastructure/pv.yml
+
+kubectl apply -f .infrastructure/pvc.yml
+ 
+kubectl apply -f .infrastructure/deployment.yml
